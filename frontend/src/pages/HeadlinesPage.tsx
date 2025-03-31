ea4a78e86936c99479a63426424fd6b29a05f899
Aya Carino <ayacarino@Aya-Carino.local>
2025-03-31 08:34:50 +0800

    added title

--- frontend/src/pages/HeadlinesPage.tsx
+++ frontend/src/pages/HeadlinesPage.tsx
@@ -1,24 +1,28 @@
 import HeadlinesCarousel from "../components/HeadlinesCarousel";
 import CategoryHeadlines from "../components/CategoryHeadlines";
+import { Typography } from "@mui/material";
 export default function HeadlinesPage() {
-	const categories = [
-		"Business",
-		"Entertainment",
-		"General",
-		"Health",
-		"Science",
-		"Sports",
-		"Technology",
-	];
+  const categories = [
+    "Business",
+    "Entertainment",
+    "General",
+    "Health",
+    "Science",
+    "Sports",
+    "Technology",
+  ];
 
-	return (
-		<>
-			<HeadlinesCarousel />
-			<div className="w-full flex flex-col gap-5 mt-5 ">
-				{categories.map((category) => (
-					<CategoryHeadlines key={category} category={category} />
-				))}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Typography variant="h3" component="h2" fontWeight="bold" sx={{ mb: 1 }}>
+        Top Headlines
+      </Typography>
+      <HeadlinesCarousel />
+      <div className="w-full flex flex-col gap-5 mt-5 ">
+        {categories.map((category) => (
+          <CategoryHeadlines key={category} category={category} />
+        ))}
+      </div>
+    </>
+  );
 }
